c55dc0884cce2da7f714f85e7af6c2247cf799bb
Lubomir1998 <lubcho_sk8@abv.bg>
2021-03-14 02:06:40 +0200

    Fifth Commit

--- src/Application.kt
+++ src/Application.kt
@@ -2,13 +2,16 @@ package com.example
 
 import com.example.data.checkIfPasswordIsCorrect
 import com.example.routes.*
-import io.ktor.application.*
-import io.ktor.auth.*
-import io.ktor.features.*
-import io.ktor.gson.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.basic
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.routing.Routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
